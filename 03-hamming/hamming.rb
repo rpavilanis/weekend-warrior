91003a2e52ab7f53b21bcddc78dbe7ce23c29fa5
Rachel <rachel.l.pavilanis@gmail.com>
2016-09-12 09:12:07 -0700

    simplified - took away == portion and made a != portion

--- 03-hamming/hamming.rb
+++ 03-hamming/hamming.rb
@@ -1,13 +1,11 @@
 class Hamming
   def self.compute(strings1, strings2)
-    hamming_distance = 0
     if strings1.length != strings2.length
       raise ArgumentError
     end
+    hamming_distance = 0
     strings1.chars.each_with_index do |string1, i|
-      if string1 == strings2.chars[i]
-        hamming_distance += 0
-      else
+      if string1 != strings2.chars[i]
         hamming_distance += 1
       end
     end
